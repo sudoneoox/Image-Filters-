5c471be70bff8c1ed6fc59cd70d04200fd84624b
DiegoLaptop <diegoa2992@gmail.com>
2023-11-30 14:31:46 -0600

    project struct

---
+++ helpers/helpers.h
@@ -0,0 +1,6 @@
+#ifndef HELPERS_H
+#define HELPERS_H
+#include "./bmp.h"
+
+
+#endif
---
+++ src/main.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+
+int main(){
+    int h,w;
+    std::cout << "Height?" << std::endl;
+    std::cin >> h;
+    std::cout << "Width?"  << std::endl;
+    std::cin >> w;
+
+    return 0;
+}
